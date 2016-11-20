d87a5ce61f0919b70ccdc3beadb6a8419840d2dd
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2016-11-20 12:39:05 -0300

    Integrate API data

--- src/ArtistList.js
+++ src/ArtistList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
   ListView,
+  ActivityIndicator,
+  StyleSheet,
 } from 'react-native';
 
 import ArtistBox from './ArtistBox'
@@ -8,18 +10,44 @@ import ArtistBox from './ArtistBox'
 export default class ArtistList extends Component {
   constructor(props) {
     super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      artistDataSource: ds.cloneWithRows(props.artists),
+      artistDataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     };
   }
 
+  updateDataSource(newData) {
+    this.setState({
+      artistDataSource: this.state.artistDataSource.cloneWithRows(newData)
+    })
+  }
+
+  componentDidMount() {
+    this.updateDataSource(this.props.artists)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.artists !== this.props.artists) {
+      this.updateDataSource(nextProps.artists)
+    }
+  }
+
   render() {
+    if (this.props.artists.length === 0) {
+      return <ActivityIndicator color="black" size="large" style={styles.loading} />
+    }
+
     return (
       <ListView
+        enableEmptySections={true}
         dataSource={this.state.artistDataSource}
         renderRow={artist => <ArtistBox artist={artist} />}
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loading: {
+    marginTop: 20,
+  }
+})
